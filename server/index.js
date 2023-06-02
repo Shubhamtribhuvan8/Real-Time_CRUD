fb66db82cffd4f7f97bf217c14f2adb4746981d4
Shubham Tribhuvan <shubhamtribhuvan8@gmail.com>
2023-06-02 20:49:11 +0530

    socket.io installation completed

--- server/index.js
+++ server/index.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const connection = require("./Config/db");
 const dotenv = require("dotenv");
-dotenv.config();
+const cors = require("cors");
 const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
-app.listen(process.env.PORT, () => {
-  try {
-    connection();
-    console.log("Server is Running on", `${process.env.PORT}`);
-  } catch (err) {
-    console.log(err);
-  }
+dotenv.config();
+connection();
+
+app.use(express.json());
+app.use(cors());
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+});
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
